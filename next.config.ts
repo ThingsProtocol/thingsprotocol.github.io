412ec59106cdf2b0a6dc287c13cf7eea8f988256
xyd945 <yudi@xyd.me>
2025-04-23 18:09:45 +0200

    fixing css file path

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,24 @@
 import type { NextConfig } from "next";
 
+const repoName = 'thingsprotocol.github.io';
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = '/'
+let basePath = ''
+
+if (isGithubActions) {
+  assetPrefix = `/${repoName}/`
+  basePath = `/${repoName}`
+}
+
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath: '',
+  basePath: basePath,
+  assetPrefix: assetPrefix,
 };
 
 export default nextConfig;
